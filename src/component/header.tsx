c325531d242bae570d727897ff6c6caf9e4cf0d0
Cemafio <cesarm10102003@gmail.com>
2026-09-06 11:15:44 +0000

    Responsive header

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -15,7 +15,7 @@ function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50">
-      <div className="flex h-14 bg-slate-900/80 backdrop-blur border border-slate-800 items-center justify-between px-8 rounded-3xl m-4 mx-20 text-slate-200">
+      <div className="flex h-14 bg-slate-900/80 backdrop-blur border border-slate-800 items-center justify-between px-8 rounded-3xl m-4 mx-10 text-slate-200">
         <span className="m-2 font-bold font-['Space_Grotesk',sans-serif] text-white">
           C-sarm
         </span>
