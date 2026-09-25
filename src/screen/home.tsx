13458ac90773b3914b512f642564340d0a36f541
Cemafio <cesarm10102003@gmail.com>
2026-09-25 07:55:43 +0000

    link linkedin and github added

--- src/screen/home.tsx
+++ src/screen/home.tsx
@@ -91,10 +91,10 @@ function HomePage() {
         </div>
 
         <div className="flex gap-4 mt-8 justify-center">
-            <a href="#" target="_blank" rel="noreferrer" aria-label="GitHub" className="text-slate-500 hover:text-teal-400">
+            <a href="https://github.com/Cemafio?tab=overview&from=2025-12-01&to=2025-12-31" target="_blank" rel="noreferrer" aria-label="GitHub" className="text-slate-500 hover:text-teal-400">
             <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.1.79-.25.79-.56 0-.27-.01-1.17-.02-2.12-3.2.7-3.88-1.36-3.88-1.36-.52-1.34-1.28-1.7-1.28-1.7-1.05-.72.08-.71.08-.71 1.16.08 1.77 1.19 1.77 1.19 1.03 1.77 2.71 1.26 3.37.96.1-.75.4-1.26.73-1.55-2.55-.29-5.23-1.28-5.23-5.68 0-1.26.45-2.28 1.19-3.09-.12-.29-.52-1.46.11-3.05 0 0 .97-.31 3.18 1.18a10.9 10.9 0 0 1 5.79 0c2.2-1.49 3.17-1.18 3.17-1.18.64 1.59.24 2.76.12 3.05.74.81 1.18 1.83 1.18 3.09 0 4.41-2.69 5.38-5.25 5.67.41.36.78 1.07.78 2.15 0 1.55-.01 2.8-.01 3.18 0 .31.21.67.8.56A10.93 10.93 0 0 0 23.5 12c0-6.35-5.15-11.5-11.5-11.5Z"/></svg>
             </a>
-            <a href="#" target="_blank" rel="noreferrer" aria-label="LinkedIn" className="text-slate-500 hover:text-teal-400">
+            <a href="https://www.linkedin.com/in/cesar-marcellin-957b29275/" target="_blank" rel="noreferrer" aria-label="LinkedIn" className="text-slate-500 hover:text-teal-400">
             <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1s2.48 1.12 2.48 2.5ZM.5 8.25h4V23h-4V8.25Zm7.5 0h3.83v2.02h.05c.53-1 1.84-2.06 3.79-2.06 4.05 0 4.8 2.67 4.8 6.13V23h-4v-6.7c0-1.6-.03-3.65-2.22-3.65-2.23 0-2.57 1.74-2.57 3.54V23h-4V8.25Z"/></svg>
             </a>
             <a href="mailto:cesarmarcellin@mail.com" aria-label="Email" className="text-slate-500 hover:text-teal-400">
